9510c9cd63323c4a0208de5c47eba869c2bcde9c
JuanPabloSR <juanpablosanchezrey22@gmail.com>
2026-08-21 19:33:18 -0500

    feat: implement shell application with single-spa and Tailwind CSS support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { registerApplication, start } from "single-spa";
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gym-shell';
+
+  ngOnInit(): void {
+    // 1. Registrar la aplicación
+    registerApplication({
+      name: "mf1-rutinas",
+      app: () => (window as any).System.import('mfe1-rutinas'), // Descarga el código desde el import map      
+      activeWhen: '/configuracion-rutinas'
+
+    });
+    // 2. Iniciar el motor de single-spa
+    start();
+  }
 }
